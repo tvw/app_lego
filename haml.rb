bfd11c0d8c167c701ed1ab3e787a724138616273
root <root@staging.tink.com.br>
2009-03-05 13:47:58 -0300

    HACK HACK HACK Installing haml-2.1 via command line

--- haml.rb
+++ haml.rb
@@ -1,5 +1,9 @@
-gem 'haml', :version => '>= 2.0'
+gem 'haml', :version => '>= 2.1'
 gem 'chriseppstein-compass', :lib => 'compass', :version => '>= 0.3.4'
+
+run "cd tmp; git clone git://github.com/nex3/haml.git; cd haml;"
+run "sudo rake install"
+run "cd ..; rm -rf haml; cd .."
 
 rake "gems:install"
 
