72b12f1cecc0354fdc9182cb7acaabf73e5bb2e8
root <root@staging.tink.com.br>
2009-03-05 13:54:39 -0300

    fixing last commit

--- haml.rb
+++ haml.rb
@@ -1,9 +1,8 @@
 gem 'haml', :version => '>= 2.1'
 gem 'chriseppstein-compass', :lib => 'compass', :version => '>= 0.3.4'
 
-run "cd tmp; git clone git://github.com/nex3/haml.git; cd haml;"
-run "sudo rake install"
-run "cd ..; rm -rf haml; cd .."
+run "git clone git://github.com/nex3/haml.git tmp/haml; cd tmp/haml; sudo rake install"
+run "rm -rf haml"
 
 rake "gems:install"
 
