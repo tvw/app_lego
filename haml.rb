7bd9ac4d3f1c1957712bbf326eff1701347060b4
Elomar França <elomar@maisweb.org>
2009-03-21 21:41:43 -0300

    compass default framework is blueprint. defining the default layout with the compass head

--- haml.rb
+++ haml.rb
@@ -15,9 +15,20 @@ end
 rake "gems:install", :sudo => true
 
 run "haml --rails ."
-framework = ask("wich framework you want to use? defaults to blueprint") || "blueprint"
+
+framework = ask("wich framework you want to use? defaults to blueprint") 
+
+framework ||= "blueprint"
 
 system "compass --rails -f #{framework} ."
+
+file "app/views/layouts/application.html.haml", <<-HAML
+%head
+  = stylesheet_link_tag 'compiled/screen.css', :media => 'screen, projection'
+  = stylesheet_link_tag 'compiled/print.css', :media => 'print'
+  /[if IE]
+    = stylesheet_link_tag 'compiled/ie.css', :media => 'screen, projection'
+HAML
 
 git :add => "."
 git :commit => "-a -m 'Added haml for views and compass for css'"
