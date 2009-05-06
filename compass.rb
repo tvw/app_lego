278dee09f459c42e0ad95ba64a5fca166c6860b9
Lars E. Hoeg <leh@lenio.dk>
2009-05-06 11:52:58 +0200

    Fixed Compass default framework and messages

--- compass.rb
+++ compass.rb
@@ -1,12 +1,9 @@
 gem 'chriseppstein-compass', :lib => 'compass', :version => '>= 0.3.4'
 
-run "echo -e 'y\nn\n' | compass --rails -f blueprint"
+framework = (ENV['COMPASS_FRAMEWORK'] || ask('which framework you want to use? [blueprint | yui] (defaults to blueprint)'))
+framework = "blueprint" if framework.nil? || framework.empty?
 
-framework = ask("wich framework you want to use? defaults to blueprint")
-
-framework ||= "blueprint"
-
-system "compass --rails -f #{framework} ."
+run "echo -e 'y\ny\n' | compass --rails -f #{framework} ."
 
 file "app/views/layouts/application.html.haml", <<-HAML
 %head
@@ -17,4 +14,4 @@ file "app/views/layouts/application.html.haml", <<-HAML
 HAML
 
 git :add => "."
-git :commit => "-a -m 'Added compass for css'"
+git :commit => "-a -m 'Added compass for css using #{framework} framework'"
