152ccd575574deb070ad5ffeadcddb9179e37266
Lars E. Hoeg <leh@lenio.dk>
2009-05-06 11:53:42 +0200

    Compass requires Haml module so install if missing

--- compass.rb
+++ compass.rb
@@ -1,3 +1,11 @@
+unless File.exists?('vendor/plugins/haml')
+  log "info", "Compass requires Haml module; will install it now"
+  templ = "#{@base_path}/haml.rb"
+  log "applying", templ
+  load_template(templ)
+  log "applied", templ
+end
+
 gem 'chriseppstein-compass', :lib => 'compass', :version => '>= 0.3.4'
 
 framework = (ENV['COMPASS_FRAMEWORK'] || ask('which framework you want to use? [blueprint | yui] (defaults to blueprint)'))
