50da8556376b6613f3859b637d5bc89c811d4322
Gustavo Barrón <gustavo@foobarra.com>
2009-05-10 01:43:32 -0500

    Fixed typo

--- factory_girl.rb
+++ factory_girl.rb
@@ -3,4 +3,4 @@ gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.g
 rake "gems:install", :sudo => true
 
 git :add => '.'
-git :commit => "-a -m 'Waiting for a girl who's got curlers in her hair; Waiting for a girl, she has no money anywhere; We get buses everywhere; Waiting for a factory girl...'"
+git :commit => "-a -m 'Waiting for a girl who\'s got curlers in her hair; Waiting for a girl, she has no money anywhere; We get buses everywhere; Waiting for a factory girl...'"
