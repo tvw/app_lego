88c36730a5f6c79237e423d92a3263db1f37c889
lhoeg <larshoeg@gmail.com>
2009-05-19 14:21:32 +0200

    install javascripts and commit changes

--- jrails.rb
+++ jrails.rb
@@ -1,3 +1,13 @@
 plugin "jrails", :git => "git://github.com/aaronchi/jrails"
 plugin "jrails_in_place_editing", :git => "git://github.com/rakuto/jrails_in_place_editing"
 
+rake 'jrails:install:javascripts'
+#rake 'jrails:inplace:install:javascripts' # script missing from plugin so we need to copy manually...
+js_dir = "javascripts"
+js_file = "jquery.inplace.pack.js"
+dest_file = File.join('public', js_dir, js_file)
+src_file = File.join('vendor', 'plugins', 'jrails_in_place_editing', js_dir, js_file)
+FileUtils.cp(src_file, dest_file)
+
+git :add => "."
+git :commit => "-m 'Installed JRails'"
