b01e31afaccb3516e9886461779fc904333b18b3
lhoeg <larshoeg@gmail.com>
2009-05-19 14:13:10 +0200

    ignore lib for rspec and rspec-rails gems to stop autotest complaining

--- rspec.rb
+++ rspec.rb
@@ -1,13 +1,13 @@
-gem 'rspec', :env => 'test'
-gem 'rspec-rails', :env => 'test'
-
-rake 'gems:install', :sudo=>true, :env => "test"
+gem 'rspec', :lib => false, :env => :test
+gem 'rspec-rails', :lib => false, :env => :test
+rake 'gems:install', :sudo=>true, :env => :test
 
 generate "rspec"
 
 # remove test dir
 git :rm => '-r test'
 
+# change default rspec format option from progress to specdoc
 file "spec/spec.opts", <<-OPTS
 --colour
 --format specdoc
@@ -16,4 +16,4 @@ file "spec/spec.opts", <<-OPTS
 OPTS
 
 git :add => "."
-git :commit => "-a -m 'Added RSpec for testing'"
+git :commit => "-m 'Added RSpec for testing'"
