915c7add21b6100bbae2fad7d51e866a3241e361
Alexey Noskov <alexey.noskov@gmail.com>
2009-08-09 21:31:04 +0400

    Fixed machinist module

--- machinist.rb
+++ machinist.rb
@@ -1,18 +1,24 @@
-plugin 'machinist', :git => 'git://github.com/notahat/machinist.git'
+gem 'notahat-machinist', :lib => 'machinist', :source => "http://gems.github.com", :env => :test
 
 git :rm => '-r spec/fixtures'
 run "mkdir spec/blueprints"
 
 file 'spec/blueprints_helper.rb', <<-BP
-You can set Sham defaults here
-Sham.name { faker or forgery }
+require 'machinist/active_record'
+require 'faker'
+require 'sham'
+
+Sham.name  { Faker::Name.name }
+Sham.email { Faker::Internet.email }
+Sham.title { Faker::Lorem.sentence }
+Sham.body  { Faker::Lorem.paragraph }
   
 Dir[File.expand_path(File.dirname(__FILE__)) + "/blueprints/**/*_blueprint.rb"].each do |blueprint|
   require blueprint
 end
 BP
 
-gsub_file 'spec/spec_helper.rb', /(require 'spec\/rails'.*)/, "\\1\nrequire 'spec/blueprints_helper.rb'"
+gsub_file 'spec/spec_helper.rb', /(defined?(RAILS_ROOT).*)/, "\\1\nrequire File.dirname(__FILE__) + '/blueprints_helper'"
 
 git :add => "."
 git :commit => "-m 'Added machinist. fixtures aren`t fun.'"
